7c2f9b9a14279ce524f57af15126022e8b3975c7
Adrian <Adrian@DESKTOP-4VGDVD8>
2018-02-07 12:54:52 +0100

    1ºDia en clase

---
+++ JA07ProyectoAgenda/src/Pagina.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author Adrian
+ */
+public class Pagina {
+    private int dia;
+    private int mes;
+    
+    private ArrayList<Cita> citas=new ArrayList<>();
+
+    public Pagina(int dia, int mes) {
+        this.dia = dia;
+        this.mes = mes;
+    }
+    
+    public void anadirCita(Cita c){
+        this.citas.add(c);
+    }
+    
+    public void borrarCita(Cita c){
+        for (Cita cita: citas) {
+            if(cita.getHora()==c.getHora() && cita.getMinutos()==c.getMinutos()){
+                this.citas.remove(cita);
+                
+            }
+        }       
+    }
+}
